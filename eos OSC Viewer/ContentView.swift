3ba5030f3ca89575be87e4c091948349be742d36
Emmett-Boedeker <ejboedeker@gmail.com>
2022-04-29 14:16:52 -0500

    Initial Commit

---
+++ eos OSC Viewer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  eos OSC Viewer
+//
+//  Created by Emmett Boedeker on 4/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ eos OSC Viewer/eos_OSC_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  eos_OSC_ViewerApp.swift
+//  eos OSC Viewer
+//
+//  Created by Emmett Boedeker on 4/29/22.
+//
+
+import SwiftUI
+
+@main
+struct eos_OSC_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
